0d6f8a7ff322371181ad58ca4f46c05fd512c9dc
Сергій Радчук <111866879+BagaBoom@users.noreply.github.com>
2023-03-03 16:23:00 +0200

    update

--- app/src/main/java/com/example/interesting_fact_about_numbers/AppDB.kt
+++ app/src/main/java/com/example/interesting_fact_about_numbers/AppDB.kt
@@ -1,17 +1,11 @@
 package com.example.interesting_fact_about_numbers
 
-import android.app.Activity
+
 import android.app.Application
-import android.content.Intent
-import android.location.LocationManager
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
 import androidx.room.Room.databaseBuilder
-import androidx.room.migration.Migration
 import com.example.interesting_fact_about_numbers.db.MainDb
 
-public class AppDB : Application() {
+class AppDB : Application() {
     private var database: MainDb? = null
 
     companion object {
@@ -29,9 +23,7 @@ public class AppDB : Application() {
 
     }
 
-    public fun getDatabase(): MainDb? {
+    fun getDatabase(): MainDb? {
         return database
     }
-
-
 }
